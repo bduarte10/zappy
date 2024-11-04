7cfc16d55e16267ee6f2cafb66c86f0ceb839c15
Bruno Duarte <duartebruno2208@gmail.com>
2024-11-03 21:52:43 -0300

    fix: atualiza URL da API do WhatsApp para novo endpoint

--- src/app/service/whatsapp.ts
+++ src/app/service/whatsapp.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://zappy-backend.fly.dev/",
+  baseURL: "https://zappy-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
