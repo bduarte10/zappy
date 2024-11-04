0207520651dab7b59a64e2cb2d27ee66fc3bc358
Bruno Duarte <duartebruno2208@gmail.com>
2024-11-04 00:21:01 -0300

    fix: atualiza URL da API do WhatsApp para ambiente local

--- src/app/service/whatsapp.ts
+++ src/app/service/whatsapp.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://zappy.adaptable.app",
+  baseURL: "http://localhost:3001",
   headers: {
     "Content-Type": "application/json",
   },
