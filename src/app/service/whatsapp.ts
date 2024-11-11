7153ab281774ca88d80b5b5f4071fd3047ccbbf9
Bruno Duarte <duartebruno2208@gmail.com>
2024-11-11 09:02:53 -0300

    fix: atualiza URL base da API do WhatsApp para incluir protocolo HTTPS

--- src/app/service/whatsapp.ts
+++ src/app/service/whatsapp.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "zappy-backend-production.up.railway.app",
+  baseURL: "https://zappy-backend-production.up.railway.app",
   // baseURL: "http://localhost:3001",
   headers: {
     "Content-Type": "application/json",
